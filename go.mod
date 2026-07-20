d3793b71041946aacb9d45e0fe4abccb116509a2
joaquimprieto <joaquim.mpp@outlook.com>
2026-07-20 00:22:02 -0300

    Initialize Go module and add main application code for address searching

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/joaquimrafael/go-address-searcher
+
+go 1.26.4
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	cep := "01001000"
+	url := "https://viacep.com.br/ws/" + cep + "/json/"
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatalf("Failed to create request: %v", err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Request failed: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to read body: %v", err)
+	}
+
+	log.Printf("Status: %s", resp.Status)
+	log.Printf("Body: %s", string(body))
+}
