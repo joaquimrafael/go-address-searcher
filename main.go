74d0bdb83d45f570b9abdd4c9fdf4764a63286fa
joaquimprieto <joaquim.mpp@outlook.com>
2026-07-20 20:15:20 -0300

    Refactor address handling and improve error management in main application

--- main.go
+++ main.go
@@ -1,36 +1,92 @@
 package main
 
 import (
-	"io"
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 )
 
-func main() {
+type Address struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Unidade     string `json:"unidade"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Estado      string `json:"estado"`
+	Regiao      string `json:"regiao"`
+	IBGE        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	DDD         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+func cepClient(url string) (Address, error) {
 	client := &http.Client{
 		Timeout: 10 * time.Second,
 	}
 
-	cep := "01001000"
-	url := "https://viacep.com.br/ws/" + cep + "/json/"
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		log.Fatalf("Failed to create request: %v", err)
+		return Address{}, fmt.Errorf("failed to create request: %v", err)
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatalf("Request failed: %v", err)
+		return Address{}, fmt.Errorf("request failed: %v", err)
+	}
+
+	if resp.StatusCode != 200 {
+		return Address{}, fmt.Errorf("invalid url %v", url)
 	}
 
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Failed to read body: %v", err)
+	var address Address
+
+	if err := json.NewDecoder(resp.Body).Decode(&address); err != nil {
+		return Address{}, fmt.Errorf("failed to decode JSON: %v", err)
 	}
 
-	log.Printf("Status: %s", resp.Status)
-	log.Printf("Body: %s", string(body))
+	return address, nil
+}
+
+func BuscaCEP(cep string) (Address, error) {
+	url := "https://viacep.com.br/ws/" + cep + "/json/"
+	address, err := cepClient(url)
+	if err != nil {
+		return Address{}, fmt.Errorf("failed to search cep %s: %w", cep, err)
+	}
+
+	return address, nil
+}
+
+func BuscaVarios(ctx context.Context, ceps []string) ([]Address, error) {
+	addresses := make([]Address, 0, len(ceps))
+	for _, v := range ceps {
+		address, err := BuscaCEP(v)
+		if err != nil {
+			return addresses, err
+		}
+		addresses = append(addresses, address)
+	}
+	return addresses, nil
+}
+
+func main() {
+	address, err := BuscaCEP("01001000")
+	if err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
+	fmt.Println("Address:", address)
+
+	addresses, err := BuscaVarios(context.TODO(), []string{"08780170", "08710190"})
+	if err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
+	fmt.Println("Addresses:", addresses)
 }
